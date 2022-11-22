c7148c583675a5cc5821dbefa548ae67fc87a9fa
José Nascimento <jose.dori.nascimento@gmail.com>
2022-11-22 15:21:02 -0300

    remove unused script

--- run.sh
+++
@@ -1,4 +0,0 @@
-docker run -it --rm \
--v /hahomes/jnascimento/exps/2022-is-al/datasets/our_events/BangladeshFire:/home/app/static/dataset \
--v /hahomes/jnascimento/exps/2022-annota-interface/annotadb:/home/annotadb \
--p 30193:30193 jnascimento/annotapi /bin/sh
